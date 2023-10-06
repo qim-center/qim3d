79da814fa6148f1c67093d293d2ac3e1688f6ca0
fima <fima@dtu.dk>
2023-10-06 14:33:49 +0200

    Doi reference system

--- qim3d/utils/__init__.py
+++ qim3d/utils/__init__.py
@@ -2,3 +2,5 @@ from . import internal_tools
 from .models import train_model, model_summary, inference
 from .augmentations import Augmentation
 from .data import Dataset, prepare_datasets, prepare_dataloaders
+#from .doi import get_bibtex, get_reference
+from . import doi
