a030867d9dc43df4e1982da7f5605bc09dd1fe15
s184058 <s184058@student.dtu.dk>
2023-10-12 13:43:48 +0200

    Completed memory_utils + memory info in load()

--- qim3d/utils/__init__.py
+++ qim3d/utils/__init__.py
@@ -4,3 +4,4 @@ from .augmentations import Augmentation
 from .data import Dataset, prepare_datasets, prepare_dataloaders
 #from .doi import get_bibtex, get_reference
 from . import doi
+from .system import Memory
---
+++ qim3d/utils/system.py
@@ -0,0 +1,34 @@
+"""Provides tools for obtaining informaion about the system."""
+import psutil
+from qim3d.utils.internal_tools import sizeof
+from qim3d.io.logger import log
+
+
+class Memory:
+    """Class for obtaining current memory information
+
+    Attributes:
+        total (int): Total system memory in bytes
+        free (int): Free system memory in bytes
+        used (int): Used system memory in bytes
+        used_pct (float): Used system memory in percentage
+    """
+
+    def __init__(self):
+        mem = psutil.virtual_memory()
+
+        self.total = mem.total
+        self.free = mem.available
+        self.free_pct = (mem.free / mem.total) * 100
+        self.used = mem.total - mem.available
+        self.used_pct = mem.percent
+
+    def report(self):
+        log.info(
+            "System memory:\n • Total.: %s\n • Used..: %s (%s%%)\n • Free..: %s (%s%%)",
+            sizeof(self.total),
+            sizeof(self.used),
+            round(self.used_pct, 2),
+            sizeof(self.free),
+            round(self.free_pct, 2),
+        )
