d614e7bd5ca20bd633c4e08bcf7f880431da1902
s184058 <s184058@student.dtu.dk>
2023-10-26 16:23:34 +0200

    Implemented beta version of annoation tool + unit test

---
+++ qim3d/tests/gui/test_annotation_tool.py
@@ -0,0 +1,36 @@
+import qim3d
+import multiprocessing
+import time
+
+
+def test_starting_class():
+    app = qim3d.gui.annotation_tool.Interface()
+
+    assert app.title == "Annotation tool"
+
+
+def test_app_launch():
+    ip = "0.0.0.0"
+    port = 65432
+
+    def start_server(ip, port):
+        app = qim3d.gui.annotation_tool.Interface()
+        app.launch(server_name=ip, server_port=port)
+
+    proc = multiprocessing.Process(target=start_server, args=(ip, port))
+    proc.start()
+
+    # App is running in a separate process
+    # So we try to get a response for a while
+    max_checks = 5
+    check = 0
+    server_running = False
+    while check < max_checks and not server_running:
+        server_running = qim3d.utils.internal_tools.is_server_running(ip, port)
+        time.sleep(1)
+        check += 1
+
+    # Terminate tre process before assertions
+    proc.terminate()
+
+    assert server_running is True
