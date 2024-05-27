c42243468d488fefbc6dc24599e1000d9b03864c
Felipe Delestro <fima@dtu.dk>
2024-05-27 15:33:06 +0200

    0.3.5 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="qim3d",
-    version="0.3.4",
+    version="0.3.5",
     author="Felipe Delestro",
     author_email="fima@dtu.dk",
     description="QIM tools and user interfaces",
