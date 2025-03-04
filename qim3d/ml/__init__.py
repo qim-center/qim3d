cfb4707131912751b6e609612325a00ca37810e9
Anna Bøgevang Ekner <s193396@dtu.dk>
2025-03-04 09:40:32 +0100

    utility for loading checkpoint

--- qim3d/ml/__init__.py
+++ qim3d/ml/__init__.py
@@ -1,4 +1,4 @@
 from ._augmentations import Augmentation
 from ._data import Dataset, prepare_datasets, prepare_dataloaders
-from ._ml_utils import model_summary, train_model, inference
+from ._ml_utils import model_summary, train_model, load_checkpoint, test_model
 from .models import *
--- qim3d/ml/_ml_utils.py
+++ qim3d/ml/_ml_utils.py
@@ -1,5 +1,6 @@
 """ Tools performed with models."""
 
+import os
 import torch
 import numpy as np
 
@@ -16,63 +17,87 @@ def train_model(
     hyperparameters: Hyperparameters,
     train_loader: torch.utils.data.DataLoader,
     val_loader: torch.utils.data.DataLoader,
+    checkpoint_directory: str = None,
     eval_every: int = 1,
     print_every: int = 5,
     plot: bool = True,
     return_loss: bool = False,
 ) -> tuple[tuple[float], tuple[float]]:
-    """Function for training Neural Network models.
+    """Trains the specified model.
+
+    The function trains the model using the data from the training and validation data loaders, according to the specified hyperparameters.
+    Optionally, the final checkpoint of the trained model is saved as a .pth file, the loss curves are plotted, and the loss values are returned. 
 
     Args:
         model (torch.nn.Module): PyTorch model.
         hyperparameters (class): Dictionary with n_epochs, optimizer and criterion.
         train_loader (torch.utils.data.DataLoader): DataLoader for the training data.
         val_loader (torch.utils.data.DataLoader): DataLoader for the validation data.
-        eval_every (int, optional): Frequency of model evaluation. Defaults to every epoch.
-        print_every (int, optional): Frequency of log for model performance. Defaults to every 5 epochs.
-        plot (bool, optional): If True, plots the training and validation loss after the model is done training.
-        return_loss (bool, optional), If True, returns a dictionary with the history of the train and validation losses.
+        checkpoint_directory (str, optional): Directory to save model checkpoint. Default is None.
+        eval_every (int, optional): Frequency of model evaluation. Default is every epoch.
+        print_every (int, optional): Frequency of log for model performance. Default is every 5 epochs.
+        plot (bool, optional): If True, plots the training and validation loss after the model is done training. Default is True.
+        return_loss (bool, optional): If True, returns a dictionary with the history of the train and validation losses. Default is False.
 
     Returns:
-        if return_loss = True:
-            tuple:
-                train_loss (dict): Dictionary with average losses and batch losses for training loop.
-                val_loss (dict): Dictionary with average losses and batch losses for validation loop.
+        train_loss (dict): Dictionary with average losses and batch losses for training loop. Only returned when `return_loss = True`.
+        val_loss (dict): Dictionary with average losses and batch losses for validation loop. Only returned when `return_loss = True`.
 
     Example:
+        ```python	
         import qim3d
-        from qim3d.ml import train_model
 
-        # defining the model.
-        model = qim3d.ml.UNet()
+        base_path = "C:/dataset/"
+        model = qim3d.ml.models.UNet(size = 'small')
+        augmentation =  qim3d.ml.Augmentation(resize = 'crop', transform_train = 'light')
+        hyperparameters = qim3d.ml.Hyperparameters(model, n_epochs = 10)
 
-        # choosing the hyperparameters
-        hyperparameters = qim3d.ml.models.Hyperparameters(model)
+        # Set up datasets and dataloaders
+        train_set, val_set, test_set = qim3d.ml.prepare_datasets(
+            path = base_path,
+            val_fraction = 0.5,
+            model = model,
+            augmentation = augmentation
+            )
 
-        # DataLoaders
-        train_loader = MyTrainLoader()
-        val_loader = MyValLoader()
+        train_loader, val_loader, test_loader = qim3d.ml.prepare_dataloaders(
+            train_set = train_set, 
+            val_set = val_set,
+            test_set = test_set,
+            batch_size = 1,
+            )
 
-        # training the model.
-        train_loss,val_loss = qim3d.ml.train_model(model, hyperparameters, train_loader, val_loader)
+        # Train model
+        qim3d.ml.train_model(
+            model = model, 
+            hyperparameters = hyperparameters, 
+            train_loader = train_loader, 
+            val_loader = val_loader,
+            checkpoint_directory = base_path, 
+            plot = True)
+        ```
     """
+    # Get hyperparameters
     params_dict = hyperparameters()
+
     n_epochs = params_dict["n_epochs"]
     optimizer = params_dict["optimizer"]
     criterion = params_dict["criterion"]
 
-    # Choosing best device available.
+    # Choosing best device available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model.to(device)
 
-    # Avoid logging twice.
+    # Avoid logging twice
     log.propagate = False
 
+    # Set up dictionaries to store training and validation losses
     train_loss = {"loss": [], "batch_loss": []}
     val_loss = {"loss": [], "batch_loss": []}
+
     with logging_redirect_tqdm():
-        for epoch in tqdm(range(n_epochs)):
+        for epoch in tqdm(range(n_epochs), desc='Training epochs', unit='epoch'):
             epoch_loss = 0
             step = 0
 
@@ -81,7 +106,7 @@ def train_model(
             for data in train_loader:
                 inputs, targets = data
                 inputs = inputs.to(device)
-                targets = targets.to(device) #.unsqueeze(1)
+                targets = targets.to(device)
 
                 optimizer.zero_grad()
                 outputs = model(inputs)
@@ -95,10 +120,10 @@ def train_model(
                 epoch_loss += loss.detach().item()
                 step += 1
 
-                # Log and store batch training loss.
+                # Log and store batch training loss
                 train_loss["batch_loss"].append(loss.detach().item())
 
-            # Log and store average training loss per epoch.
+            # Log and store average training loss per epoch
             epoch_loss = epoch_loss / step
             train_loss["loss"].append(epoch_loss)
 
@@ -111,7 +136,7 @@ def train_model(
                 for data in val_loader:
                     inputs, targets = data
                     inputs = inputs.to(device)
-                    targets = targets.to(device) #.unsqueeze(1)
+                    targets = targets.to(device)
 
                     with torch.no_grad():
                         outputs = model(inputs)
@@ -120,10 +145,10 @@ def train_model(
                     eval_loss += loss.item()
                     step += 1
 
-                    # Log and store batch validation loss.
+                    # Log and store batch validation loss
                     val_loss["batch_loss"].append(loss.item())
 
-                # Log and store average validation loss.
+                # Log and store average validation loss
                 eval_loss = eval_loss / step
                 val_loss["loss"].append(eval_loss)
 
@@ -133,9 +158,13 @@ def train_model(
                         f"val loss: {val_loss['loss'][epoch]:.4f}"
                     )
     
-    # NOTE: Delete this again
-    # Save model checkpoint to .pth file
-    torch.save(model.state_dict(), "C:/Users\s193396/dataset/model.pth")
+    if checkpoint_directory: 
+        checkpoint_filename = f"model_{n_epochs}epochs.pth"
+        checkpoint_path = os.path.join(checkpoint_directory, checkpoint_filename)
+
+        # Save model checkpoint to .pth file
+        torch.save(model.state_dict(), checkpoint_path)
+        log.info(f"Model checkpoint saved at: {checkpoint_path}")
 
     if plot:
         plot_metrics(train_loss, val_loss, labels=["Train", "Valid."], show=True)
@@ -143,8 +172,36 @@ def train_model(
     if return_loss:
         return train_loss, val_loss
 
+def load_checkpoint(model: torch.nn.Module, checkpoint_path: str) -> torch.nn.Module:
+    """Loads a trained model checkpoint from a .pth file.
 
-def model_summary(dataloader: torch.utils.data.DataLoader, model: torch.nn.Module) -> ModelStatistics:
+    Args:
+        model (torch.nn.Module): The PyTorch model to load the checkpoint into.
+        checkpoint_path (str): The path to the model checkpoint .pth file.
+
+    Returns:
+        model (torch.nn.Module): The model with the loaded checkpoint.
+
+    Example:
+        ```python
+        import qim3d
+
+        # Instantiate model architecture
+        model = qim3d.ml.models.UNet(size = 'small')
+        checkpoint_path = "C:/dataset/model_10epochs.pth"
+
+        # Load checkpoint into model
+        model = qim3d.ml.load_checkpoint(model, checkpoint_path)
+        ```
+    """
+    model.load_state_dict(torch.load(checkpoint_path))
+    log.info(f"Model checkpoint loaded from: {checkpoint_path}")
+
+    return model
+
+def model_summary(
+        model: torch.nn.Module, 
+        dataloader: torch.utils.data.DataLoader) -> ModelStatistics:
     """Prints the summary of a PyTorch model.
 
     Args:
@@ -152,13 +209,35 @@ def model_summary(dataloader: torch.utils.data.DataLoader, model: torch.nn.Modul
         dataloader (torch.utils.data.DataLoader): The data loader used to determine the input shape.
 
     Returns:
-        str: Summary of the model architecture.
+        summary (str): Summary of the model architecture.
 
     Example:
-        model = MyModel()
-        dataloader = DataLoader(dataset, batch_size=32)
-        summary = model_summary(model, dataloader)
+        ```python
+        import qim3d
+
+        base_path = "C:/dataset/"
+        model = qim3d.ml.models.UNet(size = 'small')
+        augmentation =  qim3d.ml.Augmentation(resize = 'crop', transform_train = 'light')
+
+        # Set up datasets and dataloaders
+        train_set, val_set, test_set = qim3d.ml.prepare_datasets(
+            path = base_path,
+            val_fraction = 0.5,
+            model = model,
+            augmentation = augmentation
+            )
+
+        train_loader, val_loader, test_loader = qim3d.ml.prepare_dataloaders(
+            train_set = train_set, 
+            val_set = val_set,
+            test_set = test_set,
+            batch_size = 1,
+            )
+        
+        # Get model summary
+        summary = qim3d.ml.model_summary(model, train_loader)
         print(summary)
+        ```
     """
     images, _ = next(iter(dataloader))
     batch_size = tuple(images.shape)
@@ -166,51 +245,84 @@ def model_summary(dataloader: torch.utils.data.DataLoader, model: torch.nn.Modul
 
     return model_s
 
-
-def inference(
-        data: torch.utils.data.Dataset, 
+def test_model(
         model: torch.nn.Module,
+        test_set: torch.utils.data.Dataset, 
         threshold: float = 0.5,
         ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """Performs inference on input data using the specified model.
 
-    Performs inference on the input data using the provided model. The input data should be in the form of a list,
-    where each item is a tuple containing the input image tensor and the corresponding target label tensor.
-
+    The input data should be in the form of a list, where each item is a tuple containing the input image tensor and the corresponding target label tensor.
     The function checks the format and validity of the input data, ensures the model is in evaluation mode,
-    and generates predictions using the model. The input images, target labels, and predicted labels are returned
-    as a tuple.
+    and generates predictions using the model. The input images, target labels, and predicted labels are returned as a tuple.
 
     Args:
-        data (torch.utils.data.Dataset): A Torch dataset containing input image and
-            ground truth label data.
-        model (torch.nn.Module): The trained network model used for predicting segmentations.
+        model (torch.nn.Module): The trained model used for predicting segmentations.
+        test_set (torch.utils.data.Dataset): A test dataset containing input images and ground truth label data.
         threshold (float): The threshold value used to binarize the model predictions.
 
     Returns:
-        tuple: A tuple containing the input images, target labels, and predicted labels.
+        results (list): List of tuples (volume, target, pred) containing the input images, target labels, and predicted labels.
 
     Raises:
         ValueError: If the data items are not tuples or data items do not consist of tensors.
         ValueError: If the input image is not in (C, H, W) format.
 
     Notes:
-        - The function does not assume the model is already in evaluation mode (model.eval()).
+        - The function assumes that the model is not already in evaluation mode (`model.eval()`).
 
     Example:
+        ```python	
         import qim3d
-        dataset = MySegmentationDataset()
-        model = MySegmentationModel()
-        qim3d.ml.inference(data,model)
+
+        base_path = "C:/dataset/"
+        model = qim3d.ml.models.UNet(size = 'small')
+        augmentation =  qim3d.ml.Augmentation(resize = 'crop', transform_train = 'light')
+        hyperparameters = qim3d.ml.Hyperparameters(model, n_epochs = 10)
+
+        # Set up datasets and dataloaders
+        train_set, val_set, test_set = qim3d.ml.prepare_datasets(
+            path = base_path,
+            val_fraction = 0.5,
+            model = model,
+            augmentation = augmentation
+            )
+
+        train_loader, val_loader, test_loader = qim3d.ml.prepare_dataloaders(
+            train_set = train_set, 
+            val_set = val_set,
+            test_set = test_set,
+            batch_size = 1,
+            )
+
+        # Train model
+        qim3d.ml.train_model(
+            model = model, 
+            hyperparameters = hyperparameters, 
+            train_loader = train_loader, 
+            val_loader = val_loader,
+            plot = True)
+
+        # Test model
+        results = qim3d.ml.test_model(
+            model = model, 
+            test_set = test_set
+            )
+        
+        # Get the result of the first test image
+        volume, target, pred = results[0]
+        qim3d.viz.slices_grid(pred, num_slices = 5)
+        ```
     """
     # Set model to evaluation mode
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
     model.eval()
 
+    # List to store results
     results = []
 
-    for volume, target in data:
+    for volume, target in test_set:
         if not isinstance(volume, torch.Tensor) or not isinstance(target, torch.Tensor):
             raise ValueError("Data items must consist of tensors")
 
@@ -224,17 +336,19 @@ def inference(
             output = model(volume)
 
             # Convert logits to probabilities [0, 1]
-            preds = torch.sigmoid(output)
+            pred = torch.sigmoid(output)
 
             # Convert to binary mask by thresholding the probabilities
-            preds = (preds > threshold).float()
+            pred = (pred > threshold).float()
 
             # Remove batch and channel dimensions
             volume = volume.squeeze().cpu().numpy()
             target = target.squeeze().cpu().numpy()
-            preds = preds.squeeze().cpu().numpy()
+            pred = pred.squeeze().cpu().numpy()
 
+        # TODO: Compute DICE score between target and prediction
+        
         # Append results to list
-        results.append((volume, target, preds))
+        results.append((volume, target, pred))
 
     return results
