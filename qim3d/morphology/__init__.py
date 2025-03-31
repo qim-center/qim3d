723689716682c81366ccc627bd647e9c1bc6345b
s214735 <s214735@dtu.dk>
2025-03-31 14:16:01 +0200

    ruff warning fix

---
+++ qim3d/morphology/__init__.py
@@ -0,0 +1,12 @@
+"""Morphological functions."""
+
+from ._common_morphologies import (
+    black_tophat,
+    closing,
+    dilate,
+    erode,
+    opening,
+    white_tophat,
+)
+
+__all__ = ['black_tophat', 'closing', 'dilate', 'erode', 'opening', 'white_tophat']
