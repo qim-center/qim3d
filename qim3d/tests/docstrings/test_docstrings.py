19fb2ddd356d0186ad783509bae3053f6439933d
Anna Bøgevang Ekner <s193396@dtu.dk>
2025-04-08 14:49:18 +0200

    added docstring unit tests for remaining modules

--- qim3d/tests/docstrings/test_docstrings.py
+++ qim3d/tests/docstrings/test_docstrings.py
@@ -2,7 +2,7 @@ import pytest
 import matplotlib
 from unittest.mock import patch, MagicMock
 
-from qim3d.tests import get_all_functions_by_module, check_docstring
+from qim3d.tests import get_all_functions_by_module, check_docstring, temp_data
 
 matplotlib.use('Agg')
 
@@ -52,6 +52,7 @@ def mock_io_functions():
 # Mock the qim3d.io OME-Zarr functions
 @pytest.fixture
 def mock_ome_zarr_functions(tmp_path):
+    
     # Temporary directory for mock files
     mock_file_path = tmp_path / "Escargot.zarr"
 
@@ -84,10 +85,7 @@ def mock_ome_zarr_functions(tmp_path):
 @pytest.fixture
 def mock_viz_chunks():
     with patch("qim3d.viz.chunks") as MockVizChunks:
-
-        # Mock the function to do nothing
         MockVizChunks.return_value = None
-
         yield MockVizChunks
 
 @pytest.mark.parametrize('func', functions_by_module["io"], ids=lambda d: d.__name__)
@@ -102,6 +100,45 @@ def test_docstrings_generate(func):
 def test_docstrings_viz(func, mock_downloader, mock_ome_zarr_functions, mock_viz_chunks):
     check_docstring(obj=func)
 
+@pytest.mark.parametrize('func', functions_by_module["features"], ids=lambda d: d.__name__)
+def test_docstrings_features(func):
+    check_docstring(obj=func)
+
+@pytest.mark.parametrize('func', functions_by_module["filters"], ids=lambda d: d.__name__)
+def test_docstrings_filters(func):
+    check_docstring(obj=func)
+
+@pytest.mark.parametrize('func', functions_by_module["detection"], ids=lambda d: d.__name__)
+def test_docstrings_detection(func):
+    check_docstring(obj=func)
+
+@pytest.mark.parametrize('func', functions_by_module["segmentation"], ids=lambda d: d.__name__)
+def test_docstrings_segmentation(func):
+    check_docstring(obj=func)
+
+@pytest.mark.parametrize('func', functions_by_module["operations"], ids=lambda d: d.__name__)
+def test_docstrings_operations(func):
+    check_docstring(obj=func)
+
+@pytest.mark.parametrize('func', functions_by_module["processing"], ids=lambda d: d.__name__)
+def test_docstrings_processing(func):
+    
+    # Exclude qim3d.processing.segment_layers function, since it uses unavailable example data
+    if func.__name__ == "segment_layers":
+        return
+    check_docstring(obj=func)
+
 @pytest.mark.parametrize('func', functions_by_module["mesh"], ids=lambda d: d.__name__)
 def test_docstrings_mesh(func):
     check_docstring(obj=func)
+
+@pytest.mark.parametrize('func', functions_by_module["ml"], ids=lambda d: d.__name__)
+def test_docstrings_ml(func):
+
+    # Exclude train_model, load_checkpoint, and test_model functions
+    if func.__name__ in ["train_model", "load_checkpoint", "test_model"]:
+        return
+
+    temp_data(folder='dataset', img_shape=(32, 32, 32), n=5)
+    check_docstring(obj=func)
+    temp_data(folder='dataset', remove=True)
