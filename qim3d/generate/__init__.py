0898f01c1b0bbce9705dc6fb6e420977b843e35f
s214735 <s214735@dtu.dk>
2025-05-02 13:17:02 +0200

    add test of interactive paramter explorer in notebook. MOdularized the volume generation and added the newest push to main to the aggegator.

--- qim3d/generate/__init__.py
+++ qim3d/generate/__init__.py
@@ -1,6 +1,27 @@
 """Generation for synthetic datasets."""
 
 from ._aggregators import volume_collection
-from ._generators import background, volume
+from ._generators import (
+    _distances,
+    _noise,
+    _shape_noise,
+    _threshold,
+    _tube_fade,
+    background,
+    volume,
+    volume2,
+)
 
-__all__ = ['volume', 'volume_collection', 'background', '_volume', '_volume_collection']
+__all__ = [
+    'volume',
+    'volume_collection',
+    'background',
+    '_volume',
+    '_volume_collection',
+    '_noise',
+    '_distances',
+    '_shape_noise',
+    '_threshold',
+    '_tube_fade',
+    'volume2',
+]
