1abb1a4b7005a97c18686ceb7f797054fceab87b
s214735 <s214735@dtu.dk>
2025-05-05 13:17:37 +0200

    change ParameterVisualizer such that it uses generate.volume

--- qim3d/generate/__init__.py
+++ qim3d/generate/__init__.py
@@ -3,14 +3,8 @@
 from ._aggregators import volume_collection
 from ._generators import (
     ParameterVisualizer,
-    _distances,
-    _noise,
-    _shape_noise,
-    _threshold,
-    _tube_fade,
     background,
     volume,
-    volume2,
 )
 
 __all__ = [
@@ -19,11 +13,5 @@ __all__ = [
     'background',
     '_volume',
     '_volume_collection',
-    '_noise',
-    '_distances',
-    '_shape_noise',
-    '_threshold',
-    '_tube_fade',
-    'volume2',
     'ParameterVisualizer',
 ]
