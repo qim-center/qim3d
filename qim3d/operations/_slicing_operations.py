5a15081517edd15e276ead6a86c613832022b103
Alessia Saccardo <s212246@dtu.dk>
2025-06-25 10:23:27 +0200

    Add subclass Rectangularslicer to override get_slice method and allow non square slices

--- qim3d/operations/_slicing_operations.py
+++ qim3d/operations/_slicing_operations.py
@@ -1,5 +1,53 @@
 import numpy as np
 from interactive_unet.slicer import Slicer
+from scipy.ndimage import map_coordinates
+
+
+class RectangularSlicer(Slicer):
+    def get_slice(
+        self, volume: np.ndarray, width: int, length: int, axis: int = 0, order: int = 1
+    ) -> np.ndarray:
+        """Override to accept separate width/length for the slice."""
+        # compute the interpolation coords once, but allow distinct dims
+        coords = self.get_interpolation_coords(slice_width=width)
+        # coords has shape (3, width, width); we need (3, width, length)
+        # so rebuild it replacing one axis:
+        start_w = int(-np.floor(width / 2))
+        start_l = int(-np.floor(length / 2))
+        idx_w = np.linspace(start_w, start_w + width - 1, width)
+        idx_l = np.linspace(start_l, start_l + length - 1, length)
+        # basis vectors:
+        v = self.v[:, None, None]
+        w = self.w[:, None, None]
+        origin = self.origin[:, None, None]
+        # build new 3×width×length coords:
+        x = v * idx_w[None, :, None] + w * idx_l[None, None, :] + origin
+        y = (
+            self.u[:, None, None] * idx_w[None, :, None]
+            + w * idx_l[None, None, :]
+            + origin
+        )
+        z = (
+            self.u[:, None, None] * idx_w[None, :, None]
+            + self.v[:, None, None] * idx_l[None, None, :]
+            + origin
+        )
+        new_coords = np.stack([x, y, z], axis=0)
+        # interpolate
+        if volume.ndim > 3:
+            # color channels last
+            return np.moveaxis(
+                np.array(
+                    [
+                        map_coordinates(volume[..., c], new_coords[axis], order=order)
+                        for c in range(volume.shape[-1])
+                    ]
+                ),
+                0,
+                -1,
+            )
+        else:
+            return map_coordinates(volume, new_coords[axis], order=order)
 
 
 def get_random_slice(
@@ -23,12 +71,12 @@ def get_random_slice(
         np.random.seed(seed)
 
     # Build the slicer for this volume
-    slicer = Slicer(volume.shape)
+    slicer = RectangularSlicer(volume.shape)
 
     # Randomize orientation and origin
     slicer.randomize(sampling_mode='random')
 
     # Extract square slice
-    slice2d = slicer.get_slice(volume, slice_width=width, order=1)
+    slice2d = slicer.get_slice(volume, width=width, length=length)
 
     return slice2d
