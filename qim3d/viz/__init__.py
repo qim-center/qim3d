3278566e08421bf7aadfbd2cbc4813b6bdffdab0
Felipe Delestro <fima@dtu.dk>
2026-03-20 13:03:04 +0100

    import cleanup

--- qim3d/viz/__init__.py
+++ qim3d/viz/__init__.py
@@ -25,7 +25,6 @@ from ._metrics import grid_overview, grid_pred, plot_metrics, vol_masked
 from ._preview import image_preview
 from ._structure_tensor import vectors, vector_field_3d
 from .itk_vtk_viewer import itk_vtk
-from .volume_explorer import volume_explorer
 
 __all__ = [
     '_layers2d',
