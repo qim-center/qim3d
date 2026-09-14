75c326df10ca1fbfc6052d2f0b00ffe9c8ab0670
Jeppe Klitgaard <hi@jeppe.science>
2026-09-14 14:51:29 +0200

    fix: do not initialize root logger

--- qim3d/__init__.py
+++ qim3d/__init__.py
@@ -31,12 +31,6 @@ if _TYPE_CHECKING:
     from qim3d import tests as tests
     from qim3d import utils as utils
     from qim3d import viz as viz
-import logging
-
-from qim3d import examples as examples
-from qim3d._log import initialize_logger as _initialize_logger
-
-_initialize_logger(logging.INFO, detailed=True)
 
 
 class _LazyLoader:
